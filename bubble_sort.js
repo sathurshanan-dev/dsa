e897abfd35cb5e6b413593ccea3298b87210c015
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2025-12-10 13:25:09 +1100

    fix typo

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,10 +1,9 @@
 const bubble_sort = (arr, n) => {
-  let swapped;
   for (let i = 0; i < n - 1; i++) {
-    swapped = false;
+    let swapped = false;
     for (let j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        temp = arr[j];
+        const temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
         swapped = true;
